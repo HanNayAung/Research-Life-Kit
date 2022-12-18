b346ed49a03f92ed1289b4ffce0657c4a16f90c8
HanNayAung <hus50851@kwansei.ac.jp>
2022-12-18 20:10:45 +0900

    doing

--- lib/research_life_kit.rb
+++ lib/research_life_kit.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
 # Need to Install 'watir' , 'webdrivers', 'nokogiri' gems
-# sudo gem install webdrivers
 
 require_relative "research_life_kit/version"
 require 'watir'
@@ -16,46 +15,8 @@ require 'nokogiri'
 # end
 
 
-# require 'net/http'
-# Net::HTTP.start('www.rubyinside.com') do |http|
-#   req = Net::HTTP::Get.new('/test.txt')
-#   puts http.request(req).body
-# end
-
-# require 'net/http'
-# url = URI.parse('https://ieeexplore.ieee.org.kwansei.remotexs.co/search/searchresult.jsp?newsearch=true&queryText=ICN')
-# Net::HTTP.start(url.host, url.port) do |http|
-#   req = Net::HTTP::Get.new(url.path)
-#   puts http.request(req).body
-# end
-
-
-# require 'net/http'
-# # url = URI.parse('https://ieeexplore.ieee.org.kwansei.remotexs.co/search/searchresult.jsp?newsearch=true&queryText=ICN')
-
-# # response = Net::HTTP.get_response(url)
-# # puts response.body
-
-# uri = URI.parse('https://ieeexplore.ieee.org.kwansei.remotexs.co/search/searchresult.jsp?newsearch=true&queryText=ICN')
-# http = Net::HTTP.new(uri.host, uri.port)
-# request = Net::HTTP::Get.new(uri.request_uri)
-# request.basic_auth("hus50851", "5v7bJ5ND")
-# response = http.request(request)
-# puts response
-
-# Net::HTTP.start(url.host, url.port) do |http|
-#   req = Net::HTTP::Get.new(url.path)
-#   req.basic_auth('hus50851', '5v7bJ5ND')
-#   puts http.request(req).body
-# end
-
-
-
-#browser.close
-
-
-
 browser = Watir::Browser.new
+browser.window.maximize
 browser.goto 'https://ieeexplore.ieee.org.kwansei.remotexs.co/Xplore/home.jsp'
 browser.text_field(id:'username').set 'hus50851'
 browser.text_field(id:'password').set '5v7bJ5ND'
@@ -70,35 +31,95 @@ sleep 5
 parsed_page = Nokogiri::HTML(browser.html)
 File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
 
-
 title_arrays =[]
-
-# if class name is space like "class = HI Han" then,
-# you use to do like this  .css('.HI.Han')
-
-# parsed_page.css('.col.result-item-align').css('a').map do |element|
-#   contents = element.text
-#   array_info.push(contents)
-# end
-
 
 parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
   contents = element.text
   title_arrays.push(contents)
 end
 
+for title in title_arrays  do
+  puts "#{title}"
+end
 
+
+###
+
+browser.scroll.to :bottom
+# browser.scroll.to [0, 2000]
+
+
+browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:text => '2').click
+sleep 5
+
+parsed_page = Nokogiri::HTML(browser.html)
+File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
+
+parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+  contents = element.text
+  title_arrays.push(contents)
+end
 
 for title in title_arrays  do
   puts "#{title}"
 end
 
-  #extracted_dvi.css('a')
-  #char_element = char_element.css('a')
-  #char_element.map {|element| element["_ngcontent-xcc"]}
-  #char_element.map {|element| element["href"]}
+File.open("title.org", "w") { |f| f.write "#{title_arrays}" }
 
-# puts 'hello'
+
+# browser.button('//*[@id="xplMainContent"]/div[2]/div[2]/xpl-paginator/div[2]/ul/li[3]').click()
+# browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:class => "stats-Pagination_2").click
+# browser.div(:class => "pagination-bar.hide-mobile.text-base-md-lh").ul.li.a(:class => 'stats-Pagination_2').click
+# browser.a(:class => "stats-Pagination_2").click
+# browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:class => "stats-Pagination_2 active").click
+
+
+
+
+
+
+
+
+
+
+
+
+# class="stats-Pagination_2 active"
+
+# sleep 5
+# parsed_page = Nokogiri::HTML(browser.html)
+# File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
+
+
+# title_arrays =[]
+
+# parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+#   contents = element.text
+#   title_arrays.push(contents)
+# end
+
+
+#browser.button(type: 'submit').click
+# browser.frame(class: "stats-Pagination_2").click
+
+#browser.frame(:class, 'pagination-bar.hide-mobile.text-base-md-lh
+# ').div(:class, 'stats-Pagination_2').click
+
+# browser.frame(xpath: '//*[@id="xplMainContent"]/div[2]/div[2]/xpl-paginator/div[2]/ul/li[2]/a').click
+
+
+# if class name is space like "class = HI Han" then,
+# you use to do like this  .css('.HI.Han')
+# parsed_page.css('.col.result-item-align').css('a').map do |element|
+#   contents = element.text
+#   array_info.push(contents)
+# end
+
+
+#extracted_dvi.css('a')
+#char_element = char_element.css('a')
+#char_element.map {|element| element["_ngcontent-xcc"]}
+#char_element.map {|element| element["href"]}
 
 # parsed_page.css('div').css('a').each do |extracted_dvi|
 #   #extracted_dvi.css('a')
@@ -108,27 +129,21 @@ end
 #   puts extracted_dvi
 # end
 
-# puts 'hello'
-# puts 'hello'
-
 # links = parsed_page.css('_ngcontent-wgn-c121')
 # links.map {|element| element["href"]}
 # puts links
 
 
-  # File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
+# File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
 
-  # article_cards = parsed_page.xpath("//div[contains(@class, 'col result-item-align')]")
-  # article_cards.each do |card|
-  # puts article_cards
-  # title = card.xpath("div[@class='_ngcontent-adj-c121']/a/@title")
-  # link = card.xpath("div[@class='_ngcontent-adj-c121']/a/@href")
-  # puts title
-  # puts link
-  # end
-
-
-
+# article_cards = parsed_page.xpath("//div[contains(@class, 'col result-item-align')]")
+# article_cards.each do |card|
+# puts article_cards
+# title = card.xpath("div[@class='_ngcontent-adj-c121']/a/@title")
+# link = card.xpath("div[@class='_ngcontent-adj-c121']/a/@href")
+# puts title
+# puts link
+# end
 
 # https://ieeexplore.ieee.org.kwansei.remotexs.co/search/searchresult.jsp?newsearch=true&queryText=Information%5C%2FCentric%5C%2FNetworking
 # browser.goto 'https://ieeexplore.ieee.org.kwansei.remotexs.co/search/searchresult.jsp?newsearch=true&queryText=ICN'
