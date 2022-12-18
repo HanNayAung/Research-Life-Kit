b7a9ef851de25507ca5bf36a59cfba5e4b893655
HanNayAung <hus50851@kwansei.ac.jp>
2022-12-19 03:10:28 +0900

    first commit

--- lib/research_life_kit.rb
+++ lib/research_life_kit.rb
@@ -14,57 +14,120 @@ require 'nokogiri'
 #   puts "hello"
 # end
 
+class Ieice
+  attr_accessor :user, :password, :keyword, :num_of_papers
 
-browser = Watir::Browser.new
-browser.window.maximize
-browser.goto 'https://ieeexplore.ieee.org.kwansei.remotexs.co/Xplore/home.jsp'
-browser.text_field(id:'username').set 'hus50851'
-browser.text_field(id:'password').set '5v7bJ5ND'
-browser.button(id: 'login').click
+  def titles
+    title_arrays =[]
+    url = 'https://ieeexplore.ieee.org.kwansei.remotexs.co/Xplore/home.jsp'
+    browser = Watir::Browser.new
+    browser.window.maximize
+    browser.goto url
 
+    # Login
+    browser.text_field(id:'username').set user
+    browser.text_field(id:'password').set password
+    browser.button(id: 'login').click
 
-browser.text_field(xpath: '//*[@id="LayoutWrapper"]/div/div/div/div[3]/div/xpl-root/xpl-header/div/div/div/xpl-search-bar-migr/div/form/div[2]/div/div[1]/xpl-typeahead-migr/div/input').set("Information Centric Networking")
-# browser.button(:text => 'submit').click
-browser.button(type: 'submit').click
+    # Search According to Keyword
+    xpath = '//*[@id="LayoutWrapper"]/div/div/div/div[3]/div/xpl-root/xpl-header/div/div/div/xpl-search-bar-migr/div/form/div[2]/div/div[1]/xpl-typeahead-migr/div/input'
+    browser.text_field(xpath: xpath).set keyword
+    browser.button(type: 'submit').click
 
-sleep 5
-parsed_page = Nokogiri::HTML(browser.html)
-File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
+    # Insert each title into title_arrays
+    sleep 3
+    parsed_page = Nokogiri::HTML(browser.html)
+    parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+      contents = element.text
+      title_arrays.push(contents)
+    end
 
-title_arrays =[]
+    if num_of_papers >= 2
+      2.upto(num_of_papers) do |number|
+        puts number
+        browser.window.maximize
+        browser.scroll.to :bottom
+        browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:text => number.to_s).click
+        sleep 3
+        parsed_page = Nokogiri::HTML(browser.html)
 
-parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
-  contents = element.text
-  title_arrays.push(contents)
+        parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+          contents = element.text
+          title_arrays.push(contents)
+        end
+      end
+    end
+
+    for title in title_arrays  do
+      puts "#{title}"
+    end
+  end
+
+  def titles_save
+    
+  end
 end
 
-for title in title_arrays  do
-  puts "#{title}"
-end
+organization_instance = Ieice.new
+organization_instance.user = "hus50851"
+organization_instance.password = "5v7bJ5ND"
+organization_instance.keyword = "Information Centric Networking"
+organization_instance.num_of_papers =3
+puts organization_instance.titles
 
 
-###
+# browser = Watir::Browser.new
+# browser.window.maximize
+# browser.goto 'https://ieeexplore.ieee.org.kwansei.remotexs.co/Xplore/home.jsp'
+# browser.text_field(id:'username').set 'hus50851'
+# browser.text_field(id:'password').set '5v7bJ5ND'
+# browser.button(id: 'login').click
+# browser.text_field(xpath: '//*[@id="LayoutWrapper"]/div/div/div/div[3]/div/xpl-root/xpl-header/div/div/div/xpl-search-bar-migr/div/form/div[2]/div/div[1]/xpl-typeahead-migr/div/input').set("Information Centric Networking")
+# # browser.button(:text => 'submit').click
+# browser.button(type: 'submit').click
+# sleep 5
+# parsed_page = Nokogiri::HTML(browser.html)
+# File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
+# title_arrays =[]
+# parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+#   contents = element.text
+#   title_arrays.push(contents)
+# end
+# for title in title_arrays  do
+#   puts "#{title}"
+# end
 
-browser.scroll.to :bottom
-# browser.scroll.to [0, 2000]
+
+# ###
+
+# browser.scroll.to :bottom
+# # browser.scroll.to [0, 2000]
 
 
-browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:text => '2').click
-sleep 5
+# browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:text => '2').click
+# sleep 5
 
-parsed_page = Nokogiri::HTML(browser.html)
-File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
+# parsed_page = Nokogiri::HTML(browser.html)
+# File.open("parsed.html", "w") { |f| f.write "#{parsed_page}" }
 
-parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
-  contents = element.text
-  title_arrays.push(contents)
-end
+# parsed_page.css('.col.result-item-align').css('h3.text-md-md-lh').map do |element|
+#   contents = element.text
+#   title_arrays.push(contents)
+# end
 
-for title in title_arrays  do
-  puts "#{title}"
-end
+# for title in title_arrays  do
+#   puts "#{title}"
+# end
 
-File.open("title.org", "w") { |f| f.write "#{title_arrays}" }
+# File.open("title.org", "w") { |f| f.write "#{title_arrays}" }
+
+
+
+
+
+
+
+
 
 
 # browser.button('//*[@id="xplMainContent"]/div[2]/div[2]/xpl-paginator/div[2]/ul/li[3]').click()
@@ -72,16 +135,6 @@ File.open("title.org", "w") { |f| f.write "#{title_arrays}" }
 # browser.div(:class => "pagination-bar.hide-mobile.text-base-md-lh").ul.li.a(:class => 'stats-Pagination_2').click
 # browser.a(:class => "stats-Pagination_2").click
 # browser.div(:class => "pagination-bar hide-mobile text-base-md-lh").ul.li(:class => "stats-Pagination_2 active").click
-
-
-
-
-
-
-
-
-
-
 
 
 # class="stats-Pagination_2 active"
